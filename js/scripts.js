5c3fceaf2bf6adf6d70cf9b0195691d0bf010704
Pavithira Manoharan <pavithira.manoharan@gmail.com>
2018-08-23 23:32:41 +0200

    Minor fixes.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ var canvasMenu = $('#canvas-menu');
 var closeCanvasMenu = $('#close-sidebar-menu');
 menu.on('click', function(e) {
     canvasMenu.addClass('open');
+    $('body').css('overflow', 'hidden'); 
 });
 closeCanvasMenu.on('click', function(e){
     canvasMenu.removeClass('open');
+    $('body').css('overflow', 'scroll'); 
 });
